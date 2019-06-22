49333b461166fca772fe0cb8909442361aec7967
alisherKak <alisher.alish333@gmail.com>
2019-06-22 21:28:08 +0600

    Home Work 28.06.19

--- HomeWork28_06_19/MainWindow.xaml.cs
+++ HomeWork28_06_19/MainWindow.xaml.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -23,6 +26,64 @@ namespace HomeWork28_06_19
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private string GetHostNameByIp(string ip)
+        {
+            IPAddress ipAddress;
+
+            if(IPAddress.TryParse(ip, out ipAddress))
+            {
+                try
+                {
+                    return Dns.GetHostEntry(ipAddress).HostName;
+                }
+                catch (ArgumentNullException)
+                {
+                    return string.Empty;
+                }
+                catch (ArgumentException)
+                {
+                    return string.Empty;
+                }
+                catch (SocketException)
+                {
+                    return string.Empty;
+                }
+            }
+
+            return "Incorrect IP or this ip hasn't host name";
+        }
+
+        private List<IPAddress> GetIpAddressByHostName(string hostName)
+        {
+            try
+            {
+                return Dns.GetHostEntry(hostName).AddressList.ToList();
+            }
+            catch(SocketException)
+            {
+                return new List<IPAddress>();
+            }
+            catch(ArgumentOutOfRangeException)
+            {
+                return new List<IPAddress>();
+            }
+        }
+
+        private void GetHostNameButtonClick(object sender, RoutedEventArgs e)
+        {
+            hostNameTextBlock.Text = GetHostNameByIp(ipAddressTextBox.Text);
+        }
+
+        private void GetIpButtonClick(object sender, RoutedEventArgs e)
+        {
+            ipAddressTextBlock.Text = string.Empty;
+
+            foreach(IPAddress ipAddress in GetIpAddressByHostName(urlAddressTextBox.Text))
+            {
+                ipAddressTextBlock.Text += ipAddress.ToString() + "\n";
+            }
         }
     }
 }
